943c0cd8262e07be3becbc9f4ea31779db3eb5f5
Walker Williams <williamsw@msoe.edu>
2025-11-03 20:42:46 -0600

    pmod.c implemented and commented

--- pmod.c
+++ pmod.c
@@ -11,8 +11,40 @@
  *          ./pmod
  */
 
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <time.h>
+
 int main(int argc, char* argv[])
 {
-
+    // Gets current priority
+    int priority = nice(0);
+    printf("Current priority: %d\n", priority);
+    
+    // Reduces priority by 10 (increase nice value) 
+    int new_priority = nice(10);
+    if (new_priority == -1) 
+    {
+        printf("nice() did not work correctly.");
+    } 
+    else 
+    {
+        printf("New priority: %d\n", new_priority);
+    }
+    
+    // Sleep for 1,837,272,638 nanoseconds
+    struct timespec sleep;
+    sleep.tv_sec = 1;  // 1 second
+    sleep.tv_nsec = 837272638;  // 83727268 nanoseconds
+    
+    printf("Sleeping for 1,837,272,638 nanoseconds...\n");
+    if (nanosleep(&sleep, NULL) == -1) 
+    {
+        printf("Sleep did not wait 1,837,272,638 nanoseconds");
+    }
+    
+    // Goodbye message
+    printf("Goodbye! Program completed successfully.\n");
     return 0;
 }
